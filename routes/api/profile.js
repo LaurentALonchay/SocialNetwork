8c512ad837dae6d957bd278c82afaff6469969b4
Laustin <54060691+LaurentLonchay@users.noreply.github.com>
2020-11-21 19:48:09 +0100

    Get All Profiles & Profile By User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,10 +8,12 @@ const {
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-
+////////////////////////////////////////////////////////////////////////////////////////
 // @route    GET api/profile/me
 // @desc     Get current users profile
 // @access   Private
+////////////////////////////////////////////////////////////////////////////////////////
+
 router.get('/me', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({
@@ -34,9 +36,11 @@ router.get('/me', auth, async (req, res) => {
     }
 });
 
+////////////////////////////////////////////////////////////////////////////////////////
 // @route    POST api/profile
 // @desc     Create or update user profile
 // @access   Private
+////////////////////////////////////////////////////////////////////////////////////////
 router.post(
     '/',
     [
@@ -123,5 +127,45 @@ router.post(
         }
     }
 );
+
+////////////////////////////////////////////////////////////////////////////////////////
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+////////////////////////////////////////////////////////////////////////////////////////
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({
+            msg: 'Profile not found'
+        });
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({
+                msg: 'Profile not found'
+            });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
