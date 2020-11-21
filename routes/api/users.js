b39e382c147ecd86d8cf2f4bd30df566855f6bb7
Laustin <54060691+LaurentLonchay@users.noreply.github.com>
2020-11-21 14:03:42 +0100

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {
     check,
     validationResult
 } = require('express-validator');
+const User = require('../../models/User');
+
+
 // @route    POST api/users
 // @desc     Register User
 // @access   Public
@@ -19,15 +24,64 @@ router.post('/',
             min: 6
         })
     ],
-    (req, res) => {
+    async (req, res) => {
+
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
-            })
+            });
         }
-        res.send('Users route');
 
-    })
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({
+                email
+            });
+            if (user) {
+                res.status(400).json({
+                    errors: [{
+                        msg: 'User Already Exists'
+                    }]
+                });
+            }
+            // Get Users Gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            // Encrypt Password
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // Return JsonWebToken
+
+            res.send('User Registered');
+
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    }
+);
 
 module.exports = router;
