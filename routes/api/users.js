c9b83f3a5b78c0a1398545d7f2df1aab225a8e61
Laustin <54060691+LaurentLonchay@users.noreply.github.com>
2020-11-21 14:47:17 +0100

    JsonWebToken Implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
@@ -73,8 +75,23 @@ router.post('/',
             await user.save();
 
             // Return JsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User Registered');
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), {
+                    expiresIn: 36000000
+                }, (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                        token
+                    });
+                }
+            );
 
         } catch (err) {
             console.log(err.message);
